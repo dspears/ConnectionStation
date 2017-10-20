68be402c31a82243265ebd24da8d5df249f85ff9
David Spears <david.spears@nokia.com>
2017-10-20 11:09:13 -0500

    Added bouncing, but still contains jquery

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -110,7 +110,7 @@ export class GameComponent implements OnInit, OnDestroy{
     },2000);
   }
 
-  runAnimationTimer() {
+/*  runAnimationTimer() {
     var self = this;
     var dir = 1;
     var count = 0;
@@ -128,7 +128,106 @@ export class GameComponent implements OnInit, OnDestroy{
     };
     //setInterval(animate, 60);
     requestAnimationFrame(animate);
-  }
+  } */
+
+  runAnimationTimer() {
+        var hexChars = "ABCDEF9";
+
+        function getColor() {
+            var colorStr = "";
+
+            for (var idx = 0; idx < 6; idx++) {
+            colorStr += hexChars.charAt(Math.floor(Math.random() * hexChars.length));
+            }
+
+            return '#'+colorStr;
+        } 
+
+        $.fn.bounce = function(options) {
+            
+            var settings = $.extend({
+            speed: 8
+            }, options);
+
+            return $(this).each(function() {
+            
+            var $this = $(this),
+                $parent = $this.parent(),
+                height = $parent.height(),
+                width = $parent.width(),
+                top = Math.floor(Math.random() * (height / 2)) + height / 4,
+                left = Math.floor(Math.random() * (width / 2)) + width / 4,
+                vectorX = settings.speed * (Math.random() > 0.5 ? 1 : -1),
+                vectorY = settings.speed * (Math.random() > 0.5 ? 1 : -1);
+
+            // place initially in a random location
+            $this.css({
+                'top': top,
+                'left': left
+            }).data('vector', {
+                'x': vectorX,
+                'y': vectorY
+            });
+            
+            $this.each(function(i,e) {
+                $(e).css('background-color',getColor());
+            });
+            // make initially a random color
+            var rand = getColor();
+
+            /*
+            $('#wrapper div').html('#' + rand).css({
+                'background-color': '#' + rand
+            });
+            */
+
+            var move = function($e) {
+                
+                var offset = $e.offset(),
+                width = $e.width(),
+                height = $e.height(),
+                vector = $e.data('vector'),
+                $parent = $e.parent();
+
+                if (offset.left <= 0 && vector.x < 0) {
+                vector.x = -1 * vector.x;
+                }
+                if ((offset.left + width) >= $parent.width()) {
+                vector.x = -1 * vector.x;
+                }
+                if (offset.top <= 0 && vector.y < 0) {
+                vector.y = -1 * vector.y;
+                }
+                if ((offset.top + height) >= $parent.height()) {
+                vector.y = -1 * vector.y;
+                }
+
+                $e.css({
+                'top': offset.top + vector.y + 'px',
+                'left': offset.left + vector.x + 'px'
+                }).data('vector', {
+                'x': vector.x,
+                'y': vector.y
+                });
+                
+                setTimeout(function() {
+                move($e);
+                }, 100);
+                
+            };
+            
+            move($this);
+            });
+
+        };
+
+        $(function() {
+            $('#wrapper div').bounce({
+            'speed': 7
+            });
+        });
+
+}
 
   ngOnDestroy(){
     this.userSubscription.unsubscribe();
